7f1f6ea71f48e6c3319aa6f03fdd67f2c90a9da2
jim11662418 <51113579+jim11662418@users.noreply.github.com>
2019-05-28 05:12:48 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,125 @@
+//------------------------------------------------------------------------------------------
+// Send three digit hex numbers received from serial port as commands to the Wheelwriter.
+// Only valid hex digits are accepted, any other characters are discarded.
+//------------------------------------------------------------------------------------------
+
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <reg420.h>
+#include "uart12.h"
+#include "watchdog.h"
+
+#define FALSE 0
+#define TRUE  1
+
+sbit switch1 =  P0^0;                   // dip switch connected to pin 39 0=on; 1=off (not used)
+sbit switch2 =  P0^1;                   // dip switch connected to pin 38 0=on, 1=off (not used)
+sbit switch3 =  P0^2;                   // dip switch connected to pin 37 0=on, 1=off (not used)
+sbit switch4 =  P0^3;                   // dip switch connected to pin 36 0=on, 1=off (not used)  
+
+sbit redLED =   P0^4;                   // red LED connected to pin 35 0=on, 1=off
+sbit amberLED = P0^5;                   // amber LED connected to pin 34 0=on, 1=off
+sbit greenLED = P0^6;                   // green LED connected to pin 33 0=on, 1=off
+
+code char title[]     = "DS89C440 \"Send Commands to Wheelwriter\" Version 1.0.0";
+code char compiled[]  = "Compiled " __DATE__ " at " __TIME__;
+code char copyright[] = "Copyright 2018 Jim Loos";
+
+code char instructions[] = "Enter three hex digits followed by <CR>. For example: 121<CR>";
+
+//-----------------------------------------------------------
+// main()
+//-----------------------------------------------------------
+void main(){
+    unsigned int loopcounter;           // counter for green LED
+    unsigned char c,cnt;                // character received from serial0 and count of characters
+    char line[5];                       // 4 character (plus null) string which contains input from serial0
+    char *lineptr;                      // working pointer
+    char *start = line;                 // pointer to the start of 'line'
+
+    disable_watchdog();
+    
+	EA = 1;    			                // Enable Global Interrupt Flag
+
+    PMR |= 0x01;                        // enable internal SRAM MOVX memory
+
+    greenLED = 1;                       // turn off the green LED connected to T0 (pin 14)
+    amberLED = 1;                       // turn off the amber LED connected to INT1 (pin 13)
+
+    init_serial0(9600);                 // initialize serial 0 for mode 1 at 9600bps
+    init_serial1();                     // initialize serial 1 for mode 2      
+
+    printf("\r\n\n%s\r\n%s\r\n%s\r\n\n",title,compiled,copyright);
+    switch (WDCON & 0x44) {
+        case 0x00:
+            printf("External reset\r\n");
+            break;
+        case 0x04:
+            printf("Watchdog reset\r\n");
+            break;
+        case 0x40:
+            printf("Power on reset\r\n");
+            break;
+        default:
+            printf("Unknown reset\r\n");
+    } // switch (WDCON & 0x44)
+
+    clr_flags();                                            // clear watchdog reset and POR flags for next time
+    init_watchdog(3);                                       // WD interval = 5592.4 milliseconds
+    enable_watchdog();                                      // enable watchdog reset
+
+    printf("\n%s\r\n",instructions);
+    putchar('>');                                          // prompt for input
+
+    lineptr = start;                                        // initialize pointer to beginning of 'line'
+    cnt = 0;                                                // and the count
+            
+    while(TRUE) {
+
+        if (!loopcounter++) {                               // every 65536 times through the loop (at about 2Hz)...
+            reset_watchdog();                               // "pet" the watchdog
+            greenLED = !greenLED;                           // toggle the green LED
+        }
+
+	    if (WWdata_avail()) {                               // if there's data from the Wheelwriter...
+            putchar(0x08);                                  // back up the cursor one position
+            printf(".%03X\n",get_WWdata());                 // print the data as three hex digits preceded by"."
+            putchar('>');                                   // prompt for input
+	    }
+
+        if (char_avail()) {                                 // if there's a character from the serial port...
+            c=getchar();                                    // retrieve the character
+
+            if (c==0x0D) {                                  // carriage return?
+                if (cnt) {                                  // if there's something in the buffer
+                    putchar(c);                             // echo it
+                    *lineptr = 0;                           // terminate the string with null
+                    amberLED = 0;                           // turn on the amber LED
+                    send_WWdata((int)strtol(line,NULL,16)); // send it as a 9 bit command to the Wheelwriter
+                    amberLED = 1;                           // turn off the amber LED
+                    lineptr = start;                        // reset the pointer back to the beginning of the line
+                    cnt = 0;                                // and the count
+                    putchar('>');                           // prompt for next command to Wheelwriter
+                }
+            }
+            else if (c==0x08 || c==0x7F) {                  // backspace or delete?
+                if (cnt) {                                  // if there's anything in the string
+                    lineptr--;                              // decrement the pointer
+                    cnt--;                                  // and the count
+                    putchar(0x08);                          // backspace to the previous character
+                    putchar(' ');                           // erase it (overwrite with 'space')    
+                    putchar(0x08);                          // backspace to the blank
+                }
+            }
+            else {                                          // all other characters    
+                if (isxdigit(c) && cnt<4) {                 // if it's a valid hex digit and there's room in the string
+                    putchar(c);                             // echo the character
+                    *lineptr = c;                           // store it
+                    lineptr++;                              // increment the pointer
+                    cnt++;                                  // and the count
+                }
+            }
+        }
+    }
+}
---
+++ watchdog.h
@@ -0,0 +1,5 @@
+void clr_flags();  
+void enable_watchdog();
+void disable_watchdog();
+void reset_watchdog();
+void init_watchdog(unsigned char interval);
